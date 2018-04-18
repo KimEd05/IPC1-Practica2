f8baa087dcd138c411353797fffbe0af057f241f
kimel <kimel@DESKTOP-HOJ0TH8>
2018-04-18 17:40:05 -0600

    Características Bonus

---
+++ src/ipc1/practica2_201700507/pkg1/Bonus_201700507.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ipc1.practica2_201700507.pkg1;
+
+import java.awt.Image;
+
+/**
+ *
+ * @author kimel
+ */
+public class Bonus_201700507 {
+    
+    private Image bonus;
+    private int xB;
+    private int yB;
+    private int cantidad;
+
+    public Image getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(Image bonus) {
+        this.bonus = bonus;
+    }
+
+    public int getxB() {
+        return xB;
+    }
+
+    public void setxB(int xB) {
+        this.xB = xB;
+    }
+
+    public int getyB() {
+        return yB;
+    }
+
+    public void setyB(int yB) {
+        this.yB = yB;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+    
+    
+    
+}
