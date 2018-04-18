d79504e99bc7dd8a206b196140126388319a8c79
kimel <kimel@DESKTOP-HOJ0TH8>
2018-04-18 17:39:56 -0600

    Características Disparo

---
+++ src/ipc1/practica2_201700507/pkg1/Disparo_201700507.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ipc1.practica2_201700507.pkg1;
+
+import java.awt.Image;
+
+/**
+ *
+ * @author kimel
+ */
+public class Disparo_201700507 {
+
+    private Image shoot;
+    private int xD;
+    private int yD;
+    
+    
+    public Image getShoot() {
+        return shoot;
+    }
+
+    public void setShoot(Image shoot) {
+        this.shoot = shoot;
+    }
+
+    public int getxD() {
+        return xD;
+    }
+
+    public void setxD(int xD) {
+        this.xD = xD;
+    }
+
+    public int getyD() {
+        return yD;
+    }
+
+    public void setyD(int yD) {
+        this.yD = yD;
+    }
+    
+    
+    
+}
