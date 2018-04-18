e8f4b33306356fe1e2d41ce7b0c1636efa51a899
kimel <kimel@DESKTOP-HOJ0TH8>
2018-04-18 17:39:45 -0600

    Características Enemigo

---
+++ src/ipc1/practica2_201700507/pkg1/Enemigo_201700507.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ipc1.practica2_201700507.pkg1;
+
+import java.awt.Image;
+
+/**
+ *
+ * @author kimel
+ */
+public class Enemigo_201700507 {
+    
+    private Image enemigo;
+    private int xE;
+    private int yE;
+    private int vida;
+
+    public Image getEnemigo() {
+        return enemigo;
+    }
+
+    public void setEnemigo(Image enemigo) {
+        this.enemigo = enemigo;
+    }
+
+    public int getxE() {
+        return xE;
+    }
+
+    public void setxE(int xE) {
+        this.xE = xE;
+    }
+
+    public int getyE() {
+        return yE;
+    }
+
+    public void setyE(int yE) {
+        this.yE = yE;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+    
+    
+    
+}
